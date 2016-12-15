d4949d54c23e1f5eb106b15cfd8f46c2390536a3
climoge <corentin.limoge@gmail.com>
2016-12-15 15:19:27 +0100

    Add GamePreLoader

---
+++ Possession/Assets/Scripts/Classes/GamePreLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using Possession;
+
+public class GamePreLoader : MonoBehaviour {
+
+	void Awake () {
+		GameManager gm = GameManager.Instance;
+	}
+}
