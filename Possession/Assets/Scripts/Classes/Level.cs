dc33f0d52e964335999672fb504869f0b93dffde
climoge <corentin.limoge@gmail.com>
2016-12-15 16:29:14 +0100

    Add level class

---
+++ Possession/Assets/Scripts/Classes/Level.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Possession {
+	public class Level : MonoBehaviour {
+		public string id;
+		public string nextLevel;
+		public string previousLevel;
+	}
+} // namespace Possession
