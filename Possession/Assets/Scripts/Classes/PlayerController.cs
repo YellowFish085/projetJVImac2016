7d151d0c06210c985ad78aa948127892153d35d3
Victor Viale <somebodywas@gmail.com>
2017-01-16 16:59:49 +0100

    Add PlayerController component

---
+++ Possession/Assets/Scripts/Classes/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public ZombieMovement activeZombie;
+
+    private void Awake()
+    {
+        if (!activeZombie)
+        {
+            Debug.LogError("You need to set an active Zombie");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            activeZombie.Jump();
+        }
+	}
+}
