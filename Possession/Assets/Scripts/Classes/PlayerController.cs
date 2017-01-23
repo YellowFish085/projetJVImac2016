32f0cb1f2a3f4daf4b597ffc18e61f5eb9323ab2
Victor Viale <somebodywas@gmail.com>
2017-01-23 14:50:52 +0100

    Send raw input to zombie movement to prevent inertia

--- Possession/Assets/Scripts/Classes/PlayerController.cs
+++ Possession/Assets/Scripts/Classes/PlayerController.cs
@@ -19,5 +19,8 @@ public class PlayerController : MonoBehaviour {
         {
             activeZombie.Jump();
         }
+
+        float h = Input.GetAxisRaw("Horizontal");
+        activeZombie.Move(h);
 	}
 }
