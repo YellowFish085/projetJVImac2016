20145507c6842d47e9c12022e99d434a4d6cb7c4
Pierre Charles <pierre.chls@gmail.com>
2017-02-07 14:18:14 +0100

    Update SaveManager class with JsonUtility

--- Possession/Assets/Scripts/Classes/SaveManager.cs
+++ Possession/Assets/Scripts/Classes/SaveManager.cs
@@ -1,54 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using LitJson;
 using System.IO;
 
-public class SaveManager : MonoBehaviour {
+namespace Possession
+{
+	public class SaveManager : MonoBehaviour {
 
-	// TODO: Create a SaveManager constructor with good values (currentLevel, player, etc)
+		public string currentLevelName;
 
-	// Temporary value for game progress
-	public Game game = new Game (0, "Jean-Jacques", new int[] { 3, 2, 1, 0 } );
+		void Start () {
+			
+			Debug.Log (GetCurrentLevelName ());
+		}
 
-	private string backup;
-	private string backupFilePath = "/backup.json";
-
-	public string GetBackUp()
-	{
-		return backup;
-	}
-
-	public void write () {
-		JsonData backupJSON = JsonMapper.ToJson(game);
-		File.WriteAllText (Application.dataPath + backupFilePath, backupJSON.ToString());
-		Debug.Log ("Backup file write to " + Application.dataPath);
-	}
-
-	public void read () {
-		string filePath = Application.dataPath + backupFilePath;
-		if (File.Exists (filePath)) {
-			backup = File.ReadAllText (filePath);
-		} else
+		public string GetCurrentLevelName()
 		{
-			Debug.Log ("No backup file");
+			return currentLevelName;
+		}
+
+		public void write () {
+			currentLevelName = GameManager.Instance.GetCurrentLevel ().name;
+			File.WriteAllText (Application.dataPath + "/backup.json", JsonUtility.ToJson(this));
+			Debug.Log ("Backup file write to " + Application.dataPath);
+		}
+
+		public void read () {
+			string filePath = Application.dataPath + "/backup.json";
+			if (File.Exists (filePath)) {
+				JsonUtility.FromJson<SaveManager>(filePath);
+			} else
+			{
+				Debug.Log ("No backup file");
+			}
 		}
 	}
-}
-
-
-// Temporary class to test JSONManager
-
-public class Game {
-
-	public int id;
-	public string name;
-	public int[] values;
-
-	public Game (int id, string name, int[] values) {
-		this.id = id;
-		this.name = name;
-		this.values = values;
-	}
-
 }
