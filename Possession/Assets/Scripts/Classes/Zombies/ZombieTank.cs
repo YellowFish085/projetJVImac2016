992ee08a758fbc982498090bf382add6e6405a6f
Pierre Charles <pierre.chls@gmail.com>
2017-02-14 16:46:34 +0100

    :racehorse: Implement a better strucutre with differents methods

--- Possession/Assets/Scripts/Classes/Zombies/ZombieTank.cs
+++ Possession/Assets/Scripts/Classes/Zombies/ZombieTank.cs
@@ -22,58 +22,57 @@ public class ZombieTank : MonoBehaviour {
 	private bool _isCharging = false;
 	private bool _isDestroyingTheFloor = false;
 
+	public enum Direction { Left, Right };
+	private Direction _targetedDirection;
+	private Time _timeOnStart;
+
+	public float chargeSpeed;
+
 	void Update () {
+
+		if( (Time.time - _timeOnStart) > 2*1000f) {
+			_isCharging = false;
+			_isDestroyingTheFloor = false;
+		}
+
+
 		if (_isCharging) {
-			Charge ();
+			Vector2 rgVelocity = GetComponent<Rigidbody2D> ().velocity;
+			if (_targetedDirection == Direction.Left) {
+				GetComponent<Rigidbody2D> ().velocity = new Vector2(chargeSpeed, rgVelocity.y);
+			} else if (_targetedDirection == Direction.Right) {
+				GetComponent<Rigidbody2D> ().velocity = new Vector2(-chargeSpeed, rgVelocity.y);
+			}
 		}
 		else if (_isDestroyingTheFloor) {
-			DestroyTheFloor ();
+			transform.Translate( 0f, Time.deltaTime, 0f);
 		}
 	}
 
 	void OnCollisionEnter (Collision col) {
-
 		if (_isDestroyingTheFloor && col.gameObject.name == "Ground") {
 			Destroy (col.gameObject);
 			Debug.Log (col.gameObject.name + " was destroyed");
+			_isDestroyingTheFloor = false;
 		} else if (_isCharging) {
 			Destroy (col.gameObject);
 			Debug.Log (col.gameObject.name + " was destroyed");
+			_isCharging = false;
 		}
-			
-
-		// TODO : check if gameObject could be destroyed
 	}
 
-	public void Charge () {
-		transform.Translate( Time.deltaTime, 0f, 0f);
-
-		// TODO : - set good direction with speed
-		//        - add animation on start/end
+	public void Charge (Direction direction) {
+		if (!_isCharging && !_isDestroyingTheFloor) {
+			_isCharging = true;
+			_targetedDirection = direction;
+			_timeOnStart = Time.time;
+		}
 	}
 
 	public void DestroyTheFloor () {
-		transform.Translate( 0f, -Time.deltaTime, 0f);
-
-		// TODO : - keep negative value
-		//        - set good direction with speed
-		//        - add animation on start/end
+		if (!_isDestroyingTheFloor && !_isCharging) {
+			_isDestroyingTheFloor = true;
+			_timeOnStart = Time.time;
+		}
 	}
-
-	public bool GetIsCharging () {
-		return _isCharging;
-	}
-
-	public bool GetIsDestroyingTheFloor () {
-		return _isDestroyingTheFloor;
-	}
-
-	public void SetIsCharging (bool state) {
-		_isCharging = state;
-	}
-
-	public void SetIsDestroyingTheFloor (bool state) {
-		_isDestroyingTheFloor = state;
-	}
-
 }
