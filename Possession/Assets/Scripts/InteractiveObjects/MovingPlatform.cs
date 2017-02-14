c711e86d1e9c3203bf63d80fcdb7d1b49177f93a
KrisNumber24 <assante.christophe@laposte.net>
2017-02-14 17:38:55 +0100

    Implement concrete Activator (PressurePlate) and Activable(MovingPlatform)

---
+++ Possession/Assets/Scripts/InteractiveObjects/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractiveObjects {
+
+	public class MovingPlatform : Activable<Vector3> {
+
+		// Update is called once per frame
+		// TODO Animate platform move 
+		void Update () {
+
+		}
+
+		/* Override OnValidate and set defaultValue before to call parent's OnValidate() to allow step creation in *
+		 * unity UI */
+		override public void OnValidate() {
+			this.defaultValue = GetComponent<Transform> ().position;
+			base.OnValidate ();
+		}
+
+		override
+		public void process (uint stepIdx) {
+			Debug.Log ("I move my ass to " + steps [stepIdx].x + ", " + steps [stepIdx].y);
+			GetComponent<Transform> ().position = steps [stepIdx];
+		}
+	}
+
+}
---
+++ Possession/Assets/Scripts/InteractiveObjects/PressurePlate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractiveObjects {
+	/* Example class deriving Interruptor */
+
+	public class PressurePlate : Activator {
+			
+		// Run step 1 of attachedObject when object on pressurePlate
+		void OnCollisionEnter2D(Collision2D collision) {
+			currentStep = 1;
+			this.runStep();
+		}
+
+		// Run step 0 of attachedObject when object out of pressurePlate
+		void OnCollisionExit2D(Collision2D collision) {
+			currentStep = 0;
+			this.runStep();
+		}
+			
+	}
+
+}
