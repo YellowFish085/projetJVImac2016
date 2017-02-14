c7f0e2ce12aba5ce542dd1e61432137807a7a3bf
Pierre Charles <pierre.chls@gmail.com>
2017-02-14 11:46:42 +0100

    :tada: Add getters/setters

--- Possession/Assets/Scripts/Classes/ZombieBerserkController.cs
+++ Possession/Assets/Scripts/Classes/ZombieBerserkController.cs
@@ -17,15 +17,33 @@ public class ZombieBerserkController : MonoBehaviour {
 	//    - Charge : charge something if it can be charged and/or broken
 	//    - DestroyFloor : destroy the floor if it can be broken
 
+
+
 	private bool _isCharging = false;
 	private bool _isDestroyingTheFloor = false;
 
-
 	public void Charge () {
-	
+		
 	}
 
 	public void DestroyTheFloor () {
 	
 	}
+
+	public bool GetIsCharging () {
+		return _isCharging;
+	}
+
+	public bool GetIsDestroyingTheFloor () {
+		return _isDestroyingTheFloor;
+	}
+
+	public void SetIsCharging (bool state) {
+		_isCharging = state;
+	}
+
+	public void SetIsDestroyingTheFloor (bool state) {
+		_isDestroyingTheFloor = state;
+	}
+		
 }
