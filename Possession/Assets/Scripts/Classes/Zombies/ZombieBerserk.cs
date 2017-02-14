49a6a1f00fea4b44ce4ae097e3b1b9943b1c79e6
Pierre Charles <pierre.chls@gmail.com>
2017-02-14 12:15:27 +0100

    :tada: Add Zombies folder with all special zombies scripts

---
+++ Possession/Assets/Scripts/Classes/Zombies/ZombieBerserk.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBerserk : MonoBehaviour {
+
+	// Zombie caracteristics :
+	// ----------------------
+	//    - speed : slow
+	//    - sound : muffled
+	//    - jump : can't jump
+	//    - carry : can't carry
+
+
+	// Zombie specific actions :
+	// -------------------------
+	//    - Charge : charge something if it can be charged and/or broken
+	//    - DestroyFloor : destroy the floor if it can be broken
+
+
+
+	private bool _isCharging = false;
+	private bool _isDestroyingTheFloor = false;
+
+	void Update () {
+		if (_isCharging) {
+			Charge ();
+		}
+		else if (_isDestroyingTheFloor) {
+			DestroyTheFloor ();
+		}
+	}
+
+	public void Charge () {
+		transform.Translate( Time.deltaTime, 0, 0);
+
+		// TODO : - set good direction with speed
+		//        - add animation on start/end
+	}
+
+	public void DestroyTheFloor () {
+		transform.Translate( 0, -Time.deltaTime, 0);
+
+		// TODO : - keep negative value
+		//        - set good direction with speed
+		//        - add animation on start/end
+	}
+
+	public bool GetIsCharging () {
+		return _isCharging;
+	}
+
+	public bool GetIsDestroyingTheFloor () {
+		return _isDestroyingTheFloor;
+	}
+
+	public void SetIsCharging (bool state) {
+		_isCharging = state;
+	}
+
+	public void SetIsDestroyingTheFloor (bool state) {
+		_isDestroyingTheFloor = state;
+	}
+
+}
