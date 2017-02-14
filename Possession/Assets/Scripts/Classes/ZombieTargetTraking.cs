e5b3959391b31eb026ce979794ece6d6e2688680
quentinCo <quentin.couillard@gmail.com>
2017-02-14 18:54:35 +0100

    Add offset if target is not on the same Y

--- Possession/Assets/Scripts/Classes/ZombieTargetTraking.cs
+++ Possession/Assets/Scripts/Classes/ZombieTargetTraking.cs
@@ -6,44 +6,49 @@ public class ZombieTargetTraking : MonoBehaviour {
 
     public GameObject target;
     
-    private static Vector3 rightDirection = new Vector3(1, 0, 0);
-    private static Vector3 downDirection = new Vector3(0, -1, 0);
-
-    private float currentSign = 0;
-    private bool contact = false;
-
-    // Use this for initialization
+    private bool stop = false;
+    private bool collided = false;
+    private float offsetX;
+    //private float heightObject;
+    
     void Start () {
-        //TEMP
-        target = GameObject.FindGameObjectWithTag("Scientist");
-	}
+        //heightObject = gameObject.GetComponent<Collider2D>().bounds.size.x;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if(!contact)
+        Vector3 targetPosition = target.transform.position;
+        Vector3 thisPosition = gameObject.transform.position;
+        Vector3 directionVector = targetPosition - thisPosition;
+
+        //if (Mathf.Abs(directionVector.y) > heightObject)
+        if (Mathf.Abs(directionVector.y) > 0)
+            stop = (Mathf.Abs(directionVector.x) <= offsetX) ? true : false;
+        else
+            stop = collided;
+
+        if (!stop)
         {
-            Vector3 targetPosition = target.transform.position;
-            Vector3 thisPosition = gameObject.transform.position;
-            Vector3 directionVector = targetPosition - thisPosition;
-            // Check horizontal direction.
-            float direction = Mathf.Sign(Vector3.Dot(directionVector, rightDirection));
-            if (direction != 0 && currentSign != direction)
-            {
-                currentSign = Mathf.Sign(direction);
-                Debug.Log("direction = " + currentSign);
-            }
-            gameObject.GetComponent<ZombieMovement>().Move(currentSign);
+            float direction = Mathf.Sign(directionVector.x);
+            gameObject.GetComponent<ZombieMovement>().Move(direction);
         }
+        else if(stop)
+            gameObject.GetComponent<ZombieMovement>().Move(0);
+
     }
 
     private void OnCollisionStay2D(Collision2D col)
     {
-        contact = (col.gameObject == target) ? true : false; // Oui! Parce que contact = (col.gameObject == target) ça marche.
+        collided = (col.gameObject == target) ? true : false; // Oui! Parce que stop = (col.gameObject == target) ça marche.
     }
 
     void SetTarget(GameObject newTarget)
     {
         this.target = newTarget;
+        
+        float deltaY = Mathf.Abs(target.transform.position.y - gameObject.transform.position.y);
+
+        offsetX = (deltaY > heightObject) ? Random.Range(5, 10) : 0;
     }
 
 
