54bf63a6242fa0498d93ae3c4ae36df44b29d11d
Pierre Charles <pierre.chls@gmail.com>
2017-02-14 14:19:34 +0100

    :pencil2: Change zombie name

--- Possession/Assets/Scripts/Classes/Zombies/ZombieTank.cs
+++ Possession/Assets/Scripts/Classes/Zombies/ZombieTank.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieBerserk : MonoBehaviour {
+public class ZombieTank : MonoBehaviour {
 
 	// Zombie caracteristics :
 	// ----------------------
