a88b40a9243f7a6f1c07347c340dcd729e35f8d0
Pierre Charles <pierre.chls@gmail.com>
2017-02-15 15:29:49 +0100

    :sparkles: Implement IncrementalButton feature

--- Possession/Assets/Scripts/InteractiveObjects/IncrementalButton.cs
+++ Possession/Assets/Scripts/InteractiveObjects/IncrementalButton.cs
@@ -6,8 +6,26 @@ namespace InteractiveObjects {
 
 	public class IncrementalButton : Activator {
 
+		private bool isAlreadyActive = false;
+
+		// Call NextStep method if Fire2 button is not already active
 		void Update() {
-		
+			if (!Input.GetButtonDown ("Fire2")) {
+				isAlreadyActive = true;
+				NextStep ();
+			} else {
+				isAlreadyActive = false;
+			}
+		}
+
+		// Increment step
+		private void NextStep() {
+			if(currentStep < this.attachedObject.numberOfSteps) {
+				currentStep++;
+			} else {
+				currentStep = 0;
+			}
+			this.runStep();
 		}
 
 	}
