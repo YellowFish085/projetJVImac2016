e2323d0078d0aa0fd652f59bc4273cf85c2d2bb5
Pierre Charles <pierre.chls@gmail.com>
2017-02-15 15:11:05 +0100

    :tada: Add SwitchButton class

---
+++ Possession/Assets/Scripts/Classes/SwitchButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractiveObjects {
+
+	public class SwitchButton : Activator {
+
+		// Call NextStep on collision
+		void Update() {
+			Debug.Log ("Hello");
+		}
+
+	}
+
+}
