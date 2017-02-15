5ce5ea219910c68076e9e7daaff69ee6d660f658
Pierre Charles <pierre.chls@gmail.com>
2017-02-15 14:35:34 +0100

    :dizzy: Add IncrementalPressurePlate

---
+++ Possession/Assets/Scripts/InteractiveObjects/IncrementalPressurePlate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractiveObjects {
+	/* Example class deriving Interruptor */
+
+	public class IncrementalPressurePlate : Activator {
+
+		// Increment step on input
+		private void NextStepOnInput() {
+			if(currentStep < this.attachedObject.numberOfSteps) {
+				currentStep++;
+			} else {
+				currentStep = 0;
+			}
+			this.runStep();
+		}
+
+		// Decrement step on input
+		private void PreviousStepOnInput() {
+			if(currentStep > 0) {
+				currentStep--;
+			} else {
+				currentStep = this.attachedObject.numberOfSteps;
+			}
+			this.runStep();
+		}
+
+	}
+
+}
