6fdda93414497ec83c27e4b5c8d4e71152ae7357
Pierre Charles <pierre.chls@gmail.com>
2017-02-15 15:19:42 +0100

    :zap: Set step value according to Fire1 button step

---
+++ Possession/Assets/Scripts/InteractiveObjects/SwitchButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractiveObjects {
+
+	public class SwitchButton : Activator {
+
+		// Set currentStep value according to Fire2 button state
+		void Update() {
+			if (Input.GetButtonDown ("Fire2")) {
+				currentStep = 1;
+				this.runStep ();
+			} else {
+				currentStep = 0;
+				this.runStep();
+			}
+		}
+
+	}
+
+}
