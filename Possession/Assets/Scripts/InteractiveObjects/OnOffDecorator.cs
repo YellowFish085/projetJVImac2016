ad8bf2e3fe66dcba327b5a9b782fc07b775ad0aa
KrisNumber24 <assante.christophe@laposte.net>
2017-02-15 15:38:06 +0100

    Implement a OnOffDecorator to transform any Activator into OnOffActivator

---
+++ Possession/Assets/Scripts/InteractiveObjects/OnOffDecorator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractiveObjects {
+
+	public class OnOffDecorator : Activator {
+
+		public Activator activator;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+
+		void OnValidate() {
+			if (this.activator != null && this.activator.attachedObject != null) {
+				if (this.activator.attachedObject.getMaxNumberOfSteps() != 2) {
+					this.activator.attachedObject.setMaxNumberOfSteps(2);
+					this.activator.attachedObject.OnValidate ();
+				}
+			}
+		}
+
+		public void runStep() {
+			activator.runStep ();
+		}
+	}
+
+}
