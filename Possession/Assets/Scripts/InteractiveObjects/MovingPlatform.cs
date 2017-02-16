b3101e66072c5b5a604ba6a49dd6b3ac5b06020e
KrisNumber24 <assante.christophe@laposte.net>
2017-02-16 15:42:43 +0100

    :running: Animate MovingPlatform movements

--- Possession/Assets/Scripts/InteractiveObjects/MovingPlatform.cs
+++ Possession/Assets/Scripts/InteractiveObjects/MovingPlatform.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 namespace InteractiveObjects {
 
 	public class MovingPlatform : Activable<Vector3> {
+		public float speed;
+		private Vector3 destination;
 
 		void Start() {
-
+			destination = steps [0];
 		}
 
 		// Update is called once per frame
 		// TODO Animate platform move 
 		void Update () {
-
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, destination, step);
 		}
 
 		/* Override OnValidate and set defaultValue before to call parent's OnValidate() to allow step creation in *
@@ -26,7 +29,7 @@ namespace InteractiveObjects {
 
 		override
 		public void process (uint stepIdx) {
-			GetComponent<Transform> ().position = steps [stepIdx];
+			destination = steps [stepIdx];
 		}
 	}
 
