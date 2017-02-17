af04de148806a0ce595d1d925ba117f32feb5895
quentinCo <quentin.couillard@gmail.com>
2017-02-17 12:44:06 +0100

    Base Cannibal

---
+++ Possession/Assets/Scripts/Classes/Zombies/ZombieCannibal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieCannibal : MonoBehaviour {
+
+    private GameObject target;
+    private bool locked = false;
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void SetTarget(GameObject newTarget)
+    {
+      //  controlledZombie
+    }
+}
