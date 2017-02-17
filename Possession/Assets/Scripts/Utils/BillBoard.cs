02e47a20d1a4144df5d8c0a5cdbb22dbdd1871ff
Lucas Hörand <lhorand@etud.u-pem.fr>
2017-02-17 15:33:49 +0100

    basic billboarding

---
+++ Possession/Assets/Scripts/Utils/BillBoard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour {
+
+	private Camera _camera;
+
+	void Awake (){
+		_camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>() ;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (transform.position + _camera.transform.rotation * Vector3.forward, _camera.transform.rotation * Vector3.up);
+	}
+}
