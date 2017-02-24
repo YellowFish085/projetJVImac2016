d514f1904ea972cac99728e63c4b5e0591014c65
quentinCo <quentin.couillard@gmail.com>
2017-02-24 22:20:23 +0100

    Change default layer from VoidCollision to CannibalLayer

--- Possession/Assets/Scripts/Classes/Zombies/ZombieCannibal.cs
+++ Possession/Assets/Scripts/Classes/Zombies/ZombieCannibal.cs
@@ -12,32 +12,33 @@ public class ZombieCannibal : MonoBehaviour
 
     public string zombieTagToVerify = "Zombie";
     
-    private ZombieTargetBehaviour targetBehaviour;
-    private ZombieMovement zombieMovement;
+    private ZombieTargetBehaviour targetBehaviourComponent;
+    private ZombieMovement zombieMovementComponent;
     private float heightObject;
     private float widthObject;
 
-    private int voidLayer;
+    private int itsLayer;
 
     private void Awake()
     {
-        voidLayer = LayerMask.NameToLayer("VoidCollision");
-        this.GetComponent<Collider2D>().gameObject.layer = voidLayer;
+        itsLayer = LayerMask.NameToLayer("CannibalLayer");
+        this.GetComponent<Collider2D>().gameObject.layer = itsLayer;
+        Physics2D.IgnoreLayerCollision(itsLayer, itsLayer);
     }
 
     void Start ()
     {
         heightObject = gameObject.GetComponent<Collider2D>().bounds.size.y;
         widthObject = gameObject.GetComponent<Collider2D>().bounds.size.x;
-        targetBehaviour = gameObject.GetComponent<ZombieTargetBehaviour>();
-        zombieMovement = gameObject.GetComponent<ZombieMovement>();
+        targetBehaviourComponent = gameObject.GetComponent<ZombieTargetBehaviour>();
+        zombieMovementComponent = gameObject.GetComponent<ZombieMovement>();
     }
 
     void Update ()
     {
-        if(!targetBehaviour.GetGripped())
+        if(!targetBehaviourComponent.GetGripped())
         {
-            Vector2 raycastOrigin = new Vector2(transform.position.x + zombieMovement.GetDirection() * widthObject, transform.position.y + heightObject);
+            Vector2 raycastOrigin = new Vector2(transform.position.x + zombieMovementComponent.GetDirection() * widthObject, transform.position.y + heightObject);
             checkFront(raycastOrigin);
 
             raycastOrigin = new Vector2(transform.position.x, transform.position.y + heightObject);
@@ -47,7 +48,7 @@ public class ZombieCannibal : MonoBehaviour
 
     private void checkFront(Vector2 raycastOrigin)
     {
-        Vector2 viewDirection = new Vector2(frontDetectionDistance * zombieMovement.GetDirection(), 0);
+        Vector2 viewDirection = new Vector2(frontDetectionDistance * zombieMovementComponent.GetDirection(), 0);
         int stepAngle = (int)(viewAngle / deltaDetection);
 
         GameObject targetGM = null;
@@ -66,18 +67,16 @@ public class ZombieCannibal : MonoBehaviour
             Debug.DrawRay(raycastOrigin, target);
         }
 
-        if (targetGM)
-            targetBehaviour.SetTarget(targetGM);
-                
+        this.SetTarget(targetGM);        
     }
 
     private void checkBack(Vector2 raycastOrigin)
     {
-        Vector2 backDirection = backDetectionDistance * -1 * zombieMovement.GetDirection() * Vector2.right;
+        Vector2 backDirection = backDetectionDistance * -1 * zombieMovementComponent.GetDirection() * Vector2.right;
         RaycastHit2D[] rayHits = Physics2D.RaycastAll(raycastOrigin, backDirection, backDetectionDistance);
         GameObject targetGM = this.CheckRayCast(rayHits);
-        if (targetGM)
-            targetBehaviour.SetTarget(targetGM);
+
+        this.SetTarget(targetGM);
 
         Debug.DrawRay(raycastOrigin, backDirection);
     }
@@ -88,13 +87,26 @@ public class ZombieCannibal : MonoBehaviour
         for (int j = 0; j < rayHits.Length; ++j)
         {
             float distance = Mathf.Abs(rayHits[j].point.x - transform.position.x);
-            if (distance < minDist && rayHits[j].collider.gameObject.layer != voidLayer)
+            if (distance < minDist && rayHits[j].collider.gameObject.layer != this.gameObject.layer)
             {
                 minDist = distance;
-                if (rayHits[j].collider != null && rayHits[j].collider.gameObject.tag == zombieTagToVerify) // TODO : probably change the tag with the evolution
+                if (rayHits[j].collider != null
+                    && rayHits[j].collider.gameObject.tag == zombieTagToVerify
+                    && !rayHits[j].collider.gameObject.gameObject.HasTag("Cannibal")) // TODO : probably change the tag with the evolution
+                {
                     targetGM = rayHits[j].collider.gameObject;
+                }
             }
         }
         return targetGM;
+    }
+
+    private void SetTarget(GameObject target)
+    {
+        if (!target)
+            return;
+
+        targetBehaviourComponent.SetTarget(target);
+        this.GetComponent<Collider2D>().gameObject.layer = itsLayer;
     }
 }
