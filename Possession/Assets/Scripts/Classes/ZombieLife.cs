9efc56fc654b8fe93ec288303ebac785c3b84449
quentinCo <quentin.couillard@gmail.com>
2017-02-25 00:01:50 +0100

    Add assailant save list

--- Possession/Assets/Scripts/Classes/ZombieLife.cs
+++ Possession/Assets/Scripts/Classes/ZombieLife.cs
@@ -6,19 +6,49 @@ public class ZombieLife : MonoBehaviour {
 
     public float lifePoint = 10;
 
-    public bool IncrementLife(float inc)
+    private List<GameObject> assailants; // Use when the GM die
+
+    void Awake()
     {
+        assailants = new List<GameObject>();
+    }
+
+    public void IncrementLife(float inc)
+    {
+        Debug.Log("IncrementLife");
         lifePoint += inc;
         if(lifePoint <= 0)
-        {
-            Destroy(this.gameObject);
-            return false;
-        }
-        return true;
+            FinishHimself();
     }
 
     public bool IsAlive()
     {
-        return (lifePoint <= 0);
+        return (lifePoint > 0);
+    }
+    
+    private void FinishHimself()
+    {
+        for(int i = assailants.Count - 1; i >= 0; --i)
+        {
+            ZombieCannibal zCannibal = assailants[i].GetComponent<ZombieCannibal>();
+            if (!zCannibal)
+            {
+                ZombieTargetBehaviour zTargetBehaviour = assailants[i].GetComponent<ZombieTargetBehaviour>();
+                zTargetBehaviour.SetTarget(null);
+            }
+            else
+                zCannibal.SetTarget(null);
+        }
+        Destroy(this.gameObject);
+    }
+
+    public void AddAssailant(GameObject assailant)
+    {
+        assailants.Add(assailant);
+    }
+
+    public void RemoveAssailent(GameObject assailant)
+    {
+        assailants.Remove(assailant);
     }
 }
