b618ebc459055edf7da08e69fbc435555dca549c
quentinCo <quentin.couillard@gmail.com>
2017-02-24 22:37:18 +0100

    Add ZombieLife Class

---
+++ Possession/Assets/Scripts/Classes/ZombieLife.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieLife : MonoBehaviour {
+
+    public float lifePoint = 10;
+
+    public bool IncrementLife(float inc)
+    {
+        lifePoint += inc;
+        if(lifePoint <= 0)
+        {
+            Destroy(this.gameObject);
+            return false;
+        }
+        return true;
+    }
+
+    public bool IsAlive()
+    {
+        return (lifePoint <= 0);
+    }
+}
